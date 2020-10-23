104faf83cc45f73860cc0cb404e847bb0e7a5c5c
abduamal <abduamal@ymail.com>
2020-10-23 18:04:11 -0500

    add user and account associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :accounts
+  has_many :transactions, through: :accounts
+  has_many :payments, through: :accounts
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
