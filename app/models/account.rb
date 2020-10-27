3a4bb02be522aa0574254c5de9e1556f2a1b447d
abduamal <abduamal@ymail.com>
2020-10-27 18:16:27 -0500

    add account and transaction validations

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,4 +2,7 @@ class Account < ApplicationRecord
     belongs_to :user
     has_many :transactions
     has_many :payments
+    validates :name, :account_number, presence: true
+    validates :account_number, length: { is: 9, wrong_length: "Please enter a %{count} digit account number." }
+    validates :credit_limit, length: { minimum: 1, message: "Please enter a credit limit."}
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,11 @@
 class Transaction < ApplicationRecord
   belongs_to :account
+  validates :category, inclusion: { in: %w(
+    entertainment
+    household
+    apparel
+    utilities
+    transportation
+    food
+  ), message: "Must be one of the following categories: Entertainment, Household, Apparel, Utilities, Transportation, Food"}
 end
