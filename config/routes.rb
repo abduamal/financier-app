b87ffa111f08064f4fe35b0e54a155b04c970563
abduamal <abduamal@ymail.com>
2020-10-27 20:40:30 -0500

    Incorporate first and last name attribute

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :payments
   resources :transactions
   resources :accounts
-  devise_for :users
+  #specifies the registrations controller
+  devise_for :users, :controllers => {:registrations => "registrations"}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
