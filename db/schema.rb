2a80e284d321c270bf327300a3bd8c00a8ee2c7c
abduamal <abduamal@ymail.com>
2020-10-27 19:39:03 -0500

    add name to user table

--- db/schema.rb
+++ db/schema.rb
@@ -49,6 +49,8 @@ ActiveRecord::Schema.define(version: 2020_10_23_215624) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "first_name", default: "", null: false
+    t.string "last_name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
