719923d6076b3d512d35ba6de8553c5b33a686a0
abduamal <abduamal@ymail.com>
2020-10-31 13:32:14 -0500

    add basic account controller actions

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,2 +1,56 @@
 class AccountsController < ApplicationController
+    def index
+        @accounts = Account.all
+    end
+
+    def show
+        set_account
+        render :'accounts/show'
+    end
+
+    def new
+        @account = Account.new
+    end
+
+    def create
+        @account = Account.new(account_params)
+
+        if @account.save
+            redirect_to @account
+        else
+            render :new
+        end
+    end
+
+    def edit
+        set_account
+    end
+
+    def update
+        set_account
+        @account.update(account_params)
+
+        if @account.save
+            redirect_to @account
+        else
+            render :edit
+        end
+    end
+    
+    def destroy
+        set_account
+        @account.destroy
+        flash[:notice] = "Account Deleted"
+        redirect_to accounts_path
+    end
+
+    private
+
+    def account_params
+        params.require(:account).permit(:user_id, :name, :description, :credit_limit, :balance, :account_number, :status)
+    end
+
+    def set_account
+        @account = Account.find(params[:account_id])
+    end
 end
